90ccaab2ffeae9841c93777a106d0cc01c13e099
Scott Campbell <stc9486@gmail.com>
2020-10-28 15:41:47 -0400

    post

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const http = require('http');
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false})); //will parse all body properties sent by form
+
 app.use((req, res, next) => {
+  //console.log('always runs!');
   next(); // allows request to continue in middleware chain
 })
 
 app.use('/add-product', (req, res, next) => {
-  res.send('<h1>Add Product Page</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+})
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/')
 })
 
 app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express</h1>');
+  res.send('<h1>Hello from Express</for>');
 })
 
 const server = http.createServer(app);
