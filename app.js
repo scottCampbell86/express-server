585ea0713dd4b32a9f1abc42b5f2aa7e97ad4a18
Scott Campbell <stc9486@gmail.com>
2020-10-28 15:55:06 -0400

    err

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require('http');
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
@@ -11,17 +13,12 @@ app.use((req, res, next) => {
   next(); // allows request to continue in middleware chain
 })
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express</for>');
+app.use((err, req, res, next) => {
+  if (err) res.send(err);
+  res.send('<h1>Cannot Find Page</h1>')
 })
 
 const server = http.createServer(app);
